0eb6b58ded1b924d83a5a059c2e12e01e0f13a9f
Cooper Dalrymple <dcooperdalrymple@gmail.com>
2026-01-05 16:21:29 -0600

    Manage changes independently from data in CPSAVES

--- src/launcher_config.py
+++ src/launcher_config.py
@@ -12,6 +12,17 @@ try:
 except ImportError:
     pass
 
+def _merge(data: dict, changes: dict) -> dict:
+    if data is None:
+        return changes
+    if changes is not None:
+        for key, value in changes.items():
+            if isinstance(value, dict) and key in data and isinstance(data[key], dict):
+                data[key] = _merge(data[key], value)
+            else:
+                data[key] = value
+    return data
+
 def _json_dump_pretty(data: dict|list|tuple, stream: FileIO, indent: int = 0, indent_size: int = 4) -> None:
     stream.write(("{" if isinstance(data, dict) else "[") + "\n")
 
@@ -41,19 +52,31 @@ class LauncherConfig:
 
     def __init__(self):
         self._data = {}
-        for directory in ("/", "/sd/", "/saves/"):
+        self._changes = {}
             launcher_config_path = directory + "launcher.conf.json"
             if pathlib.Path(launcher_config_path).exists():
                 with open(launcher_config_path, "r") as f:
-                    self._data = self._data | json.load(f)
+                    try:
+                        data = json.load(f)
+                    except (AttributeError, ValueError):
+                        pass
+                    else:
+                        self._data = _merge(self._data, data)
 
-    def _get_value(self, group: str, name: str, default: Any = None) -> Any:
+    def _set_value(self, name: str, value: Any) -> None:
+        self._data[name] = value
+        self._changes[name] = value
+
+    def _get_group_value(self, group: str, name: str, default: Any = None) -> Any:
         return self._data[group].get(name, default) if group in self._data else default
 
-    def _set_value(self, group: str, name: str, value: Any) -> Any:
+    def _set_group_value(self, group: str, name: str, value: Any) -> None:
         if group not in self._data:
             self._data[group] = {}
+        if group not in self._changes:
+            self._changes[group] = {}
         self._data[group][name] = value
+        self._changes[group][name] = value
 
     @property
     def data(self) -> dict:
@@ -62,6 +85,7 @@ class LauncherConfig:
     @data.setter
     def data(self, value: dict) -> None:
         self._data = value
+        self._changes = {}
 
     @property
     def use_mouse(self) -> bool:
@@ -69,7 +93,7 @@ class LauncherConfig:
     
     @use_mouse.setter
     def use_mouse(self, value: bool) -> None:
-        self._data["use_mouse"] = value
+        self._set_value("use_mouse", value)
 
     @property
     def favorites(self) -> list:
@@ -77,47 +101,47 @@ class LauncherConfig:
 
     @favorites.setter
     def favorites(self, value: list) -> None:
-        self._data["favorites"] = value
+        self._set_value("favorites", value)
 
     @property
     def palette_bg(self) -> int:
-        return int(self._get_value("palette", "bg", "0x222222"), 0)
+        return int(self._get_group_value("palette", "bg", "0x222222"), 0)
 
     @palette_bg.setter
     def palette_bg(self, value: int) -> None:
-        self._set_value("palette", "bg", "0x{:06x}".format(value))
+        self._set_group_value("palette", "bg", "0x{:06x}".format(value))
 
     @property
     def palette_fg(self) -> int:
-        return int(self._get_value("palette", "fg", "0xffffff"), 0)
+        return int(self._get_group_value("palette", "fg", "0xffffff"), 0)
 
     @palette_fg.setter
     def palette_fg(self, value: int) -> None:
-        self._set_value("palette", "fg", "0x{:06x}".format(value))
+        self._set_group_value("palette", "fg", "0x{:06x}".format(value))
 
     @property
     def palette_arrow(self) -> int:
-        return int(self._get_value("palette", "arrow", "0x004abe"), 0)
+        return int(self._get_group_value("palette", "arrow", "0x004abe"), 0)
 
     @palette_arrow.setter
     def palette_arrow(self, value: int) -> None:
-        self._set_value("palette", "arrow", "0x{:06x}".format(value))
+        self._set_group_value("palette", "arrow", "0x{:06x}".format(value))
 
     @property
     def palette_accent(self) -> int:
-        return int(self._get_value("palette", "accent", "0x008800"), 0)
+        return int(self._get_group_value("palette", "accent", "0x008800"), 0)
 
     @palette_accent.setter
     def palette_accent(self, value: int) -> None:
-        self._set_value("palette", "accent", "0x{:06x}".format(value))
+        self._set_group_value("palette", "accent", "0x{:06x}".format(value))
 
     @property
     def audio_output(self) -> str:
-        return str(self._get_value("audio", "output", "headphone"))
+        return str(self._get_group_value("audio", "output", "headphone"))
 
     @audio_output.setter
     def audio_output(self, value: str) -> None:
-        self._set_value("audio", "output", value)
+        self._set_group_value("audio", "output", value)
 
     @property
     def audio_output_speaker(self) -> bool:
@@ -129,19 +153,19 @@ class LauncherConfig:
 
     @property
     def audio_volume(self) -> float:
-        return min(max(float(self._get_value("audio", "volume", 0.35)), 0.0), 1.0)
+        return min(max(float(self._get_group_value("audio", "volume", 0.35)), 0.0), 1.0)
 
     @audio_volume.setter
     def audio_volume(self, value: float) -> None:
-        self._set_value("audio", "volume", min(max(value, 0.0), 1.0))
+        self._set_group_value("audio", "volume", min(max(value, 0.0), 1.0))
 
     @property
     def audio_volume_override_danger(self) -> float:
-        return min(max(float(self._get_value("audio", "volume_override_danger", 0.75)), 0.0), 1.0)
+        return min(max(float(self._get_group_value("audio", "volume_override_danger", 0.75)), 0.0), 1.0)
 
     @audio_volume_override_danger.setter
     def audio_volume_override_danger(self, value: float) -> None:
-        self._set_value("audio", "volume_override_danger", min(max(value, 0.0), 1.0))
+        self._set_group_value("audio", "volume_override_danger", min(max(value, 0.0), 1.0))
 
     @property
     def boot_animation(self) -> str:
@@ -178,20 +202,20 @@ class LauncherConfig:
 
     @property
     def screensaver_module(self) -> str:
-        return str(self._get_value("screensaver", "module", ""))
+        return str(self._get_group_value("screensaver", "module", ""))
     
     @screensaver_module.setter
     def screensaver_module(self, value: str) -> None:
         if self._valid_module(value):
-            self._set_value("screensaver", "module", value)
+            self._set_group_value("screensaver", "module", value)
 
     @property
     def screensaver_class(self) -> str:
-        return str(self._get_value("screensaver", "class", ""))
+        return str(self._get_group_value("screensaver", "class", ""))
     
     @screensaver_class.setter
     def screensaver_class(self, value: str) -> None:
-        self._set_value("screensaver", "class", value)
+        self._set_group_value("screensaver", "class", value)
 
     def get_screensaver(self, module_name: str = None) -> object:
         class_name = ""
@@ -215,18 +239,18 @@ class LauncherConfig:
                 
     @property
     def screensaver_timeout(self) -> int:
-        value = self._get_value("screensaver", "timeout")
+        value = self._get_group_value("screensaver", "timeout")
         if value is None:
             value = self._data.get("screensaver.timeout")  # compatibility with previous option
         return int(value) if value is not None else 30
     
     @screensaver_timeout.setter
     def screensaver_timeout(self, value: int) -> None:
-        self._set_value("screensaver", "timeout", max(value, 1))
+        self._set_group_value("screensaver", "timeout", max(value, 1))
     
     @property
     def screensaver_background_color(self) -> str:
-        value = self._get_value("screensaver", "background_color")
+        value = self._get_group_value("screensaver", "background_color")
         if value is None:
             value = self._data.get("screensaver.background_color")  # compatibility with previous option
         return str(value) if value is not None else "0x000000"
@@ -235,7 +259,7 @@ class LauncherConfig:
     def screensaver_background_color(self, value: str|int) -> None:
         if isinstance(value, int):
             value = "0x{:06x}".format(value)
-        self._set_value("screensaver", "background_color", value)
+        self._set_group_value("screensaver", "background_color", value)
 
     @staticmethod
     def can_save() -> bool:
@@ -247,9 +271,25 @@ class LauncherConfig:
             return not mount.readonly
 
     def save(self) -> bool:
+        if not self._changes:
+            return False
+        
+        # read existing config
+        data = None
+        if pathlib.Path("/saves/launcher.conf.json").exists():
+            try:
+                with open("/saves/launcher.conf.json", "r") as f:
+                    data = json.load(f)
+            except (AttributeError, ValueError, OSError, IOError):
+                pass
+        
+        # merge with changes
+        data = _merge(data, self._changes)
+
+        # save updated config
         try:
             with open("/saves/launcher.conf.json", "w") as f:
-                _json_dump_pretty(self._data, f)
+                _json_dump_pretty(data, f)
         except (OSError, IOError):
             return False
         else:
